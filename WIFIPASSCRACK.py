762e588a66f27cc32390c95bbb132b0cd0a0643d
Anbu Chelvan <89595378+8055anbu@users.noreply.github.com>
2022-09-29 21:54:55 +0400

    Add files via upload

---
+++ WIFIPASSCRACK.py
@@ -0,0 +1,76 @@
+print(input("Cracking Technique:"))
+
+print(input("Enter the WIFI SSID:"))
+
+
+
+print("[00:00:00] 4/7120712 keys testing (1600.2 k/s)")
+
+print("\n")
+
+print("Master Key     : E3 01 B1 64 21 AC F5 0C 61 AE 42 B4 BB B9 E4 98 ")
+print("                 DF D1 A5 B6 B2 BD 30 0C 68 D4 65 FD 08 7D B3 3C ")
+
+print("\n")
+
+print("Transient Key  : 82 5E 0A 2B 03 65 9C 35 BB 0A D5 54 93 D9 00 CF ")
+print("                 81 B3 0F F6 84 83 8C E3 45 58 92 E7 A5 F0 9B A1 ")
+print("                 36 F8 66 F7 D4 EF C4 AF 70 30 33 4D 49 A3 4E 2E ")
+print("                 26 E3 F2 27 FB 2E 28 6D EB 25 CA 9F DA 13 B3 44 ")
+
+print("\n")
+
+print("EAPOL HMAC     : 6A F7 C6 75 89 CC D6 C1 D2 2A DD 44 56 B0 8E 0C ")
+print("\n")
+
+
+
+from time import sleep
+from tqdm import tqdm
+for i in tqdm(range(10)):
+    sleep(2)
+    
+import colorama
+from colorama import Fore
+
+
+print('\n')
+
+print(Fore.RED + 'Cracking Failed')
+print("\n")
+
+wifipass = ("avitamnivami25")
+
+print(input("Cracking technique:"))
+
+print(input("Enter the WIFI SSID:"))
+
+
+print("BSSID               STATION             PWR     RATE      LOST     FRAME     PROBE   ")
+
+print("\n")
+
+print("(not associated)    34:C0:59:9A:C6:3F   -76     0 - 1     0        1                 ")
+print("(not associated)    EC:51:BC:1F:CE:3D   -76     0 - 1     0        1         pak aray")
+print("(not associated)    DA:A1:19:B0:F6:73   -76     0 - 1     0        1                 ")
+print("(not associated)    86:73:CA:93:51:4F   -78     0 - 1     0        1                 ")
+print("C8:D7:19:FD:12:A2   34:E6:AD:7A:15:CF   -66     0e- 0e    0        285               ")
+print("C4:6E:1F:68:5B:1A   30:3A:64:63:6F:EB   -44     0e-54e    0        22                ")
+print("C4:6E:1F:68:5B:1A   AC:CF:5C:C4:B4:C6   -48     0e- 24    300      1343              ")
+print("0C:80:63:2C:DA:C6   00:87:01:28:EE:71   -68     1e- 1e    1        266               ")
+print("E8:65:D4:47:32:08   D4:25:8B:48:C5:4C   -74     0 - 1e    0        5         Personal-WiFi")
+print("E8:65:D4:47:32:08   2C:6E:85:FC:E9:A5   -72     0 - 5E    0        3         #750P   ")
+
+print("\n")
+
+from time import sleep
+from tqdm import tqdm
+for i in tqdm(range(20)):
+    sleep(3)
+    
+print('\n')
+print(Fore.BLUE + 'Cracking Succesfull')
+print("IP Adress : 133.109.68.199")
+print("MAC Adress : ue:cf:f3:03:9a:5f")
+print(wifipass)
+print(input(""))
